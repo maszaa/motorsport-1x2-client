751ae13102453fc979891e6eaab88da5757fdc96
Samuli Kohomäki <samuli.kohomaki@gmail.com>
2017-05-08 12:04:54 +0300

    Tuned gitignore

--- app/classes/series.class.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Series = (function () {
-    function Series(theName) {
-        this.name = theName;
-    }
-    return Series;
-}());
-exports.Series = Series;
-//# sourceMappingURL=series.class.js.map
