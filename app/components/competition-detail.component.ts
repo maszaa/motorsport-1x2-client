e8c5a6e0c1950f0fbd54a5ee3c24098d96de17bf
Samuli Kohomäki <samuli.kohomaki@gmail.com>
2017-05-12 12:02:54 +0300

    Added adding of player to competition

--- app/components/competition-detail.component.ts
+++ app/components/competition-detail.component.ts
@@ -3,21 +3,34 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { Competition }  from './../classes/competition.class';
 import { CompetitionService }  from './../services/competition.service';
+import { Player }  from './../classes/player.class';
+import { PlayerService }  from './../services/player.service';
 
 @Component({
   selector: 'competition',
   templateUrl: './../templates/competition.html',
-  providers: [CompetitionService]
+  providers: [CompetitionService, PlayerService]
 })
 
 export class CompetitionDetailComponent implements OnInit {
   title = 'Competition';
   competition: Competition;
+  newPlayer: Player;
 
-  constructor(private competitionService: CompetitionService, private route:ActivatedRoute) { }
+  constructor(private competitionService: CompetitionService,
+              private playerService: PlayerService,
+              private route:ActivatedRoute) { }
 
   getCompetition(id: number): void {
     this.competitionService.getCompetition(id).then(competition => this.competition = competition);
+  }
+
+  addPlayer(name: string, competitionId: number): void {
+    if (!name || !competitionId) {
+      return;
+    }
+    this.playerService.addPlayer(name, competitionId)
+                           .then(newPlayer => this.newPlayer = newPlayer)
   }
 
   ngOnInit(): void {
--- app/services/player.service.ts
+++ app/services/player.service.ts
@@ -7,6 +7,7 @@ import { Player }  from './../classes/player.class';
 @Injectable()
 export class PlayerService {
   private url: string = 'http://127.0.0.1:8000/api/players/';
+  private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { }
 
@@ -14,6 +15,14 @@ export class PlayerService {
     return this.http.get(`${this.url}?competitionId=${competitionId}`)
                .toPromise()
                .then(response => response.json() as Player[])
+               .catch(this.handleError);
+  }
+
+  addPlayer(name: string, competitionId: number): Promise<Player> {
+    return this.http
+               .post(this.url, JSON.stringify({name: name, competition: competitionId}), {headers: this.headers})
+               .toPromise()
+               .then(response => response.json() as Player)
                .catch(this.handleError);
   }
 
